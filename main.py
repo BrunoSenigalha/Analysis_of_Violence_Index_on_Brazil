57631afdcba3807e7771d1b8a996b10f1d283238
Bruno Senigalha <brsenigalha@gmail.com>
2024-06-21 11:51:08 -0300

    adicionando novas funcionalidades e melhorias

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ sheet_name = 'Planilha2'
 
 data_xlsx = st.DataStatistic(url, sheet_name)
 statistics = data_xlsx.analyze_data()
-score_z = data_xlsx.calculate_score_z(2019)
+score_z = data_xlsx.calculate_score_z(2017)
+highest_index_value = data_xlsx.finding_highest_index()
 
+print(data_xlsx)
+print(f"O Escore Z é {score_z:.2f}")
+print(f"O ano com maior índice de violência foi {highest_index_value[0]}, com o valor de {highest_index_value[1]} \n"
+      f"Sua distância em relação à média (escore z) de {score_z:.2f}")
 
-print(f"A média é {statistics['mean']}")
-print(f"O Escore Z é {score_z}")
--- statistic_insights.py
+++ statistic_insights.py
@@ -7,19 +7,19 @@ class DataStatistic:
         self.url = url
         self.sheet_name = sheet_name
         self.violence_data = self.reading_file()
+        self.statistics = self.analyze_data()
 
     def reading_file(self):
-        violence_file = pd.read_excel(self.url, index_col=0, sheet_name=self.sheet_name).squeeze()
-        return violence_file
+        violence_data = pd.read_excel(self.url, index_col=0, sheet_name=self.sheet_name).squeeze()
+        return violence_data
 
     def analyze_data(self):
         statistics = {
             'mean': self.violence_data.mean(),
             'median': self.violence_data.median(),
-            'quartile_25': self.violence_data.quantile(0.25),
-            'quartile_50': self.violence_data.quantile(0.50),
-            'quartile_75': self.violence_data.quantile(0.75),
-            'non_null': self.violence_data.count(),
+            'Q1': self.violence_data.quantile(0.25),
+            'Q2': self.violence_data.quantile(0.50),
+            'Q3': self.violence_data.quantile(0.75),
             'std': self.violence_data.std()
         }
         return statistics
@@ -27,31 +27,23 @@ class DataStatistic:
     def calculate_score_z(self, number):
         return (self.violence_data[number] - self.violence_data.mean()) / self.violence_data.std()
 
-# class DataStatistic:
-#     def __init__(self, violence_data):
-#         self.violence_data = violence_data
-#
-#     def analyze_data(self):
-#         statistics = {
-#             'mean': self.violence_data.mean(),
-#             'median': self.violence_data.median(),
-#             'quartile_25': self.violence_data.quantile(0.25),
-#             'quartile_50': self.violence_data.quantile(0.50),
-#             'quartile_75': self.violence_data.quantile(0.75),
-#             'non_null': self.violence_data.count(),
-#             'std': self.violence_data.std()
-#         }
-#         return statistics
-#
-#     def calculate_score_z(self, number):
-#         return (self.violence_data[number] - self.violence_data.mean()) / self.violence_data.std()
-#
-#
-# class Data:
-#     def __init__(self, url, sheet_name):
-#         self.url = url
-#         self.sheet_name = sheet_name
-#
-#     def reading_file(self):
-#         violence_file = pd.read_excel(self.url, index_col=0, sheet_name=self.sheet_name).squeeze()
-#         return violence_file
+    def finding_highest_index(self):
+        index = self.violence_data.idxmax()
+        value = self.violence_data.max()
+        return [index, value]
+
+    # def outlier(self):
+    #     diq = self.statistics['Q3'] - self.statistics['Q1']
+    #     lower_bound = self.statistics['Q1'] - 1.5 * diq
+    #     upper_bound = self.statistics['Q3'] + 1.5 * diq
+    #     # outliers = self.violence_data[(self.violence_data < lower_bound) |
+    #     #                               (self.violence_data > upper_bound)]
+    #     return outliers
+
+    def __str__(self):
+        return (f"Média: {self.statistics['mean']:.2f} \n"
+                f"Mediana: {self.statistics['median']:.2f} \n"
+                f"Primeiro Quartil: {self.statistics['Q1']:.2f} \n"
+                f"Segundo Quartil: {self.statistics['Q2']:.2f} \n"
+                f"Terceiro Quartil: {self.statistics['Q3']:.2f} \n"
+                f"Desvio Padrão: {self.statistics['std']:.2f}")
